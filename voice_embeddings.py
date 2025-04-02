1843e4794e87e6fe68c21a922ebd8038ed056735
ERIEIRO-TATA <emanuel-0@hotmail.com>
2025-04-02 18:03:20 -0300

    Update voice_embeddings.py

--- voice_embeddings.py
+++ voice_embeddings.py
@@ -1,12 +1,13 @@
 from resemblyzer import VoiceEncoder, preprocess_wav
 import numpy
 
-def resemblyzer_voice_embeddings(audio_path: str) -> numpy.ndarray:
+def resemblyzer_voice_embeddings(audio_path: str, encoder) -> numpy.ndarray:
     """
-    Extracts voice embeddings from an audio file using the Resemblyzer model.
+    Extracts voice embeddings from an audio file in audio_path using the Resemblyzer model.
 
     Parameters:
         audio_path (str): Path to the input audio file.
+        encoder: resemblyzer voice encoder instance
 
     Returns:
         numpy.ndarray: A NumPy array containing the voice embedding.
@@ -14,29 +15,24 @@ def resemblyzer_voice_embeddings(audio_path: str) -> numpy.ndarray:
     
     # Preprocess the audio file (convert to the required format)
     wav = preprocess_wav(audio_path)
-    
-    # Initialize the voice encoder
-    encoder = VoiceEncoder()
     
     # Extract and return the voice embedding
     return encoder.embed_utterance(wav)
 
-def resemblyzer_speaker_embeddings(audio_path: str) -> numpy.ndarray:
+def resemblyzer_speaker_embeddings(audio_paths: list, encoder) -> numpy.ndarray:
     """
-    Extracts speaker embeddings from an audio file using the Resemblyzer model.
+    Extracts speaker embeddings from an audio file in audio_path using the Resemblyzer model.
 
     Parameters:
         audio_path (str): Path to the input audio file.
+        encoder: resemblyzer voice encoder instance
 
     Returns:
         numpy.ndarray: A NumPy array containing the voice embedding.
     """
     
     # Preprocess the audio file (convert to the required format)
-    wav = preprocess_wav(audio_path)
-    
-    # Initialize the voice encoder
-    encoder = VoiceEncoder()
+    wav = [preprocess_wav(i) for i in audio_paths]
     
     # Extract and return the speaker embedding
     return encoder.embed_speaker(wav)
